f97fef35b624f0d6bf43468149a871774233b24e
Derek Peruo <derek@derekperuo.net>
2015-06-30 21:39:20 -0400

    update skill list

--- app/js/skillList.jsx
+++ app/js/skillList.jsx
@@ -13,12 +13,12 @@ var SkillList = React.createClass({
               <li className="skill-list__item">Bootstrap 3</li>
               <li className="skill-list__item">CSS3</li>
               <li className="skill-list__item">Foundation 5</li>
-              <li className="skill-list__item">Illustrator</li>
-              <li className="skill-list__item">Jasmine</li>
               <li className="skill-list__item">JavaScript</li>
+              <li className="skill-list__item">Jira</li>
               <li className="skill-list__item">jQuery</li>
               <li className="skill-list__item">Node.js</li>
-              <li className="skill-list__item">Photoshop</li>
+              <li className="skill-list__item">npm</li>
+              <li className="skill-list__item">React.js</li>
               <li className="skill-list__item">Sass</li>
               <li className="skill-list__item"><a href="/skills"><i className="fa fa-fw fa-share"></i>More&hellip;</a></li>
             </ul>
