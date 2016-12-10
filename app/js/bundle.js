ebed00614c05eac78d403f5214d053e815e706b2
dperuo <derek@derekperuo.net>
2016-12-09 19:42:56 -0600

    Update bundle.js

--- app/js/bundle.js
+++ app/js/bundle.js
@@ -1,5 +1,5 @@
-(function(undefined) {
-  'use strict';
+((undefined) => {
 
-  console.info("Oh hi! :)")
-})();
+  console.info("Oh hi! :) Looks like you know your way around the console. We should talk about your project. derek@derekperuo.net")
+
+}());
